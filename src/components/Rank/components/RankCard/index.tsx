3db8349498a9f5faa820bc351ce5c21bf68c2a45
Yellow10041 <yellowvatral@gmail.com>
2023-11-08 19:43:28 +0200

    update

--- src/components/Rank/components/RankCard/index.tsx
+++ src/components/Rank/components/RankCard/index.tsx
@@ -1,6 +1,8 @@
 import clsx from "clsx";
 import styles from "./index.module.scss";
-import { ReactNode } from "react";
+import { ReactNode, useLayoutEffect, useRef } from "react";
+import Blub from "../../../../assets/blub";
+import gsap from "gsap";
 
 interface IRankCardProps {
   theme?: "dark" | "light";
@@ -20,28 +22,103 @@ const RankCard: React.FunctionComponent<IRankCardProps> = ({
   active = false,
   theme = "light",
 }) => {
+  const refBlob = useRef<HTMLDivElement>(null);
+  const refBlobBack = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      let tl1 = gsap.timeline({
+        defaults: {
+          delay: 0,
+          duration: 1.6,
+          ease: "power1.inOut",
+          repeat: -1,
+          yoyo: true,
+        },
+      });
+
+      let tl2 = gsap.timeline({
+        defaults: {
+          delay: 1,
+          duration: 3,
+          ease: "power1.inOut",
+          repeat: -1,
+          yoyo: true,
+        },
+      });
+
+      tl1.fromTo(
+        refBlob.current,
+        {
+          scale: 1,
+          opacity: 0.5,
+        },
+        { scale: 1.1, opacity: 0.7 },
+        "<10%"
+      );
+
+      tl2.fromTo(
+        refBlobBack.current,
+        {
+          scale: 0.6,
+          opacity: 0,
+        },
+        { scale: 1.1, opacity: 0.4 }
+      );
+      // .fromTo(
+      //   refBlob.current,
+      //   {
+      //     scale: 0.9,
+      //   },
+      //   { scale: 1 }
+      // )
+      // .fromTo(
+      //   refBlobBack.current,
+      //   {
+      //     scale: 0.9,
+      //   },
+      //   { scale: 1 }
+      // );
+    });
+
+    return () => ctx.revert();
+  }, []);
+
   return (
-    <div
-      className={clsx(
-        styles.RankCard,
-        styles[rank],
-        styles[theme],
-        active && styles.active
-      )}
-    >
-      <div className={clsx(styles.RankCard_icon)}>{icon}</div>
-      <div className={clsx(styles.RankCard_title)}>{rank}</div>
-      <div className={clsx(styles.RankCard_info)}>
-        {info.map((e, i) => (
-          <div className={clsx(styles.RankCard_info_item)} key={i}>
-            <div className={clsx(styles.RankCard_info_item_title)}>
-              {e.title}
+    <div className={clsx(styles.RankCard, active && styles.active)}>
+      <div className={clsx(styles.RankCard_blubBox)}>
+        <div className={clsx(styles.RankCard_blub)} ref={refBlob}>
+          <Blub />
+        </div>
+        <div
+          className={clsx(styles.RankCard_blub, styles.back)}
+          ref={refBlobBack}
+        >
+          <Blub />
+        </div>
+      </div>
+      <div
+        className={clsx(
+          styles.RankCard_inner,
+          styles[rank],
+          styles[theme],
+          active && styles.active
+        )}
+      >
+        <div className={clsx(styles.RankCard_icon)}>{icon}</div>
+        <div className={clsx(styles.RankCard_title)}>{rank}</div>
+        <div className={clsx(styles.RankCard_info)}>
+          {info.map((e, i) => (
+            <div className={clsx(styles.RankCard_info_item)} key={i}>
+              <div className={clsx(styles.RankCard_info_item_title)}>
+                {e.title}
+              </div>
+              <div className={clsx(styles.RankCard_info_item_value)}>
+                {e.value}
+              </div>
             </div>
-            <div className={clsx(styles.RankCard_info_item_value)}>
-              {e.value}
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
